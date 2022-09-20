21125646b442fbcd816f564c1caa02a2d085511c
HeitorGomesIF <heitorgomes500@outlook.com>
2022-09-19 22:19:36 -0300

    Commit Inicial

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+//session_start();
+
+//$conexao = new mysqli('127.0.0.1','root','','churras_dos_cria');
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include "config.php";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/custom.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
